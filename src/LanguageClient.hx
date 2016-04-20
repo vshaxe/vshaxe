fb7386dcd7705095ebea8488a53db30770fbb630
Dan Korostelev <nadako@gmail.com>
2016-04-20 21:48:31 +0300

    new externs

---
+++ src/LanguageClient.hx
@@ -0,0 +1,43 @@
+import haxe.extern.EitherType;
+import js.node.ChildProcess.ChildProcessForkOptions;
+import js.Promise;
+import Vscode;
+
+@:jsRequire("vscode-languageclient", "LanguageClient")
+extern class LanguageClient {
+    function new(name:String, serverOptions:ServerOptions, languageOptions:LanguageClientOptions, ?forceDebug:Bool);
+    function start():Disposable;
+    function stop():Void;
+    function onNotification(type:{method:String}, handler:Dynamic->Void):Void;
+    function onReady():Promise<Void>;
+}
+
+@:enum abstract TransportKind(Int) {
+    var stdio = 0;
+    var ipc = 1;
+}
+
+typedef NodeModule = {
+    var module:String;
+    @:optional var transport:TransportKind;
+    @:optional var args:Array<String>;
+    @:optional var options:ChildProcessForkOptions;
+}
+
+typedef ServerOptions = {
+    run: NodeModule,
+    debug: NodeModule,
+}
+
+typedef LanguageClientOptions = {
+    @:optional var documentSelector:EitherType<String,Array<String>>;
+    @:optional var synchronize:SynchronizeOptions;
+    @:optional var diagnosticCollectionName:String;
+    @:optional var initializationOptions:Dynamic;
+}
+
+typedef SynchronizeOptions = {
+    @:optional var configurationSection:EitherType<String,Array<String>>;
+    @:optional var fileEvents:EitherType<FileSystemWatcher,Array<FileSystemWatcher>>;
+    @:optional var textDocumentFilter:TextDocument->Bool;
+}
