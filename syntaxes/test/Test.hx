7381233586078955eed474d9d20dc8a085b3c38d
Gama11 <jensfischer95@gmail.com>
2017-01-12 15:55:59 +0100

    Proper regex highlighting

--- syntaxes/test/Test.hx
+++ syntaxes/test/Test.hx
@@ -89,7 +89,10 @@ class Foo {
 
 		null; // Unknown<0>
 
-		~/[a-z]+/i; // EReg : regular expression
+ 		// EReg : regular expression
+		~/^[1-9]\d{0,2}$/g;
+		~/^Null<(\$\d+)>$/;
+		~/^@(param|default|exception|throws|deprecated|return|returns|since|see)\s+([^@]+)/gm;
 
 		$type(2);
 
