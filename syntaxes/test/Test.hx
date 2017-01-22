a1eb0f38d7c7d9cdcd60044b2cfd66cf5836e903
Gama11 <jensfischer95@gmail.com>
2017-01-22 16:07:10 +0100

    Fix method return type hints for inline methods

--- syntaxes/test/Test.hx
+++ syntaxes/test/Test.hx
@@ -369,7 +369,9 @@ class Colors {
 
 class Foo {
 	function g() {
-		f(function() return b < c);
+		f(function():Void 0 < 1);
+		f(function():Void a < b);
+		f(function():Void<Dynamic> a < b);
 	}
 }
 
