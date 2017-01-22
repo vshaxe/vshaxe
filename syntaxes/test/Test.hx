294d6be9b8fb668cb36c98f04b76d9999b11f887
Gama11 <jensfischer95@gmail.com>
2017-01-22 22:47:58 +0100

    Fix highlighting of "new" expressions without type params

--- syntaxes/test/Test.hx
+++ syntaxes/test/Test.hx
@@ -246,6 +246,9 @@ class Foo {
 		var c = macro interface IClass {};
 		var MMap:MMap<String, String> = new MMap<String, String>();
 		var a = new A<String>(12);
+		var ct = new foo.CanvasTexture(12, "foo");
+		eq(ct.i, 12);
+		eq(ct.s, "foo");
 		
 		//macro class could have no name...
 		var def = macro class {
