e724da9b4a656c6f2ff4204b825bc55104fcee3f
Gama11 <jensfischer95@gmail.com>
2017-03-05 15:39:08 +0100

    [build] fix invalid json

--- Build.hx
+++ Build.hx
@@ -1,8 +1,6 @@
 /** Just a wrapper for build/Build.hx to avoid having to pass --cwd or -lib hxargs **/
 class Build {
     static function main() {
-        Sys.command("haxe", ["--cwd", "build", "-lib", "hxargs", "-lib", "hxnodejs", "-js", "run.js", "-main", "Main"]);
-        Sys.setCwd("build");
-        Sys.command("node", ["run.js"].concat(Sys.args()));
+        Sys.command("haxe", ["--cwd", "build", "-lib", "hxargs", "--run", "Main"].concat(Sys.args()));
     }
 }
