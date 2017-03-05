03daec14f0678514263cf93ab25eb0f0378eb120
Gama11 <jensfischer95@gmail.com>
2017-03-05 16:05:08 +0100

    [build] fix exit codes for Travis

--- Build.hx
+++ Build.hx
@@ -1,6 +1,6 @@
 /** Just a wrapper for build/Build.hx to avoid boilerplate **/
 class Build {
     static function main() {
-        Sys.command("haxe", ["--cwd", "build", "-lib", "hxargs", "-lib", "json2object", "--run", "Main"].concat(Sys.args()));
+        Sys.exit(Sys.command("haxe", ["--cwd", "build", "-lib", "hxargs", "-lib", "json2object", "--run", "Main"].concat(Sys.args())));
     }
 }
