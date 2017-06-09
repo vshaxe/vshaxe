491fccc7a172e1d34a309bcc3b95abfe3b93c784
Jens Fischer <jensfischer95@gmail.com>
2017-06-09 14:47:06 +0200

    Display the versions of haxelibs

--- src/vshaxe/HaxelibExplorer.hx
+++ src/vshaxe/HaxelibExplorer.hx
@@ -1,16 +1,30 @@
 package vshaxe;
 
+import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
 import Vscode.*;
 import vscode.*;
 import js.Promise;
+import js.node.ChildProcess;
+import js.node.Buffer;
+using StringTools;
 
 class HaxelibExplorer {
     var context:ExtensionContext;
     var configuration:Array<String>;
-    var libraries:Array<String>;
+    var libraries:Array<Haxelib>;
+    var haxelibRepo(get,never):String;
+
+    function get_haxelibRepo():String {
+        if (_haxelibRepo == null) {
+            _haxelibRepo = Path.normalize((ChildProcess.execSync('haxelib config') : Buffer).toString().trim());
+        }
+        return _haxelibRepo;
+    }
+
     var _onDidChangeTreeData = new EventEmitter<TreeItem>();
+    var _haxelibRepo:String;
 
     public var onDidChangeTreeData:Event<TreeItem>;
 
@@ -29,11 +43,58 @@ class HaxelibExplorer {
         if (hxmlFile != null && FileSystem.exists(hxmlFile)) {
             var hxml = File.getContent(hxmlFile);
             // TODO: parse the hxml properly
-            ~/-lib\s+([\w:]+)/g.map(hxml, function(ereg) {
-                libraries.push(ereg.matched(1));
+            ~/-lib\s+([\w:.]+)/g.map(hxml, function(ereg) {
+                var name = ereg.matched(1);
+                trace(name);
+                var path = getHaxelibPath(name);
+                if (path != null) {
+                    libraries.push({
+                        name: name,
+                        label: getHaxelibLabel(name, path),
+                        path: path
+                    });
+                }
                 return "";
             });
         }
+    }
+
+    function getHaxelibPath(lib:String):String {
+        try {
+            var result:Buffer = ChildProcess.execSync('haxelib path $lib');
+            var path = null;
+            for (line in result.toString().split("\n")) {
+                var potentialPath = Path.normalize(line.trim());
+                if (FileSystem.exists(potentialPath)) {
+                    path = potentialPath;
+                    break;
+                }
+            }
+            return path;
+        } catch(e:Any) {
+            return null;
+        }
+    }
+
+    function getHaxelibVersion(path:String):String {
+        if (path.indexOf(haxelibRepo) == -1) {
+            // can't assume that paths outside of haxelib follow the /<lib>/<version> naming scheme -
+            // just show the path itself for e.g. "haxelib local"
+            return path;
+        }
+
+        path = path.replace(haxelibRepo, "");
+        var segments = path.split("/");
+        var version = segments[2];
+        if (version == null) {
+            return "?";
+        }
+        return version.replace(",", ".");
+    }
+
+    function getHaxelibLabel(name:String, path:String) {
+        name = name.split(":")[0];
+        return '$name (${getHaxelibVersion(path)})';
     }
 
     public function onDisplayConfigurationChanged(configuration:Array<String>) {
@@ -50,7 +111,13 @@ class HaxelibExplorer {
             if (libraries == null) {
                 updateLibraries(configuration);
             }
-            resolve([for (library in libraries) new TreeItem(library)]);
+            resolve([for (library in libraries) new TreeItem(library.label)]);
         });
     }
+}
+
+private typedef Haxelib = {
+    name:String,
+    label:String,
+    path:String
 }
