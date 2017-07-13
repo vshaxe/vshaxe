44db69fa624cb2fb06849592c7f482c1e1fbf626
Dan Korostelev <nadako@gmail.com>
2017-07-14 00:45:06 +0300

    remove settings.json initialization feature (obsolted by hxml autodiscovery and hxml display provider)

--- src/vshaxe/commands/InitProject.hx
+++ src/vshaxe/commands/InitProject.hx
@@ -19,76 +19,14 @@ class InitProject {
             return;
         }
 
-        var emptyOrOnlyHiddenFiles = FileSystem.readDirectory(workspaceRoot).foreach(function(f) return f.startsWith("."));
-        if (emptyOrOnlyHiddenFiles) {
-            scaffoldEmpty(workspaceRoot);
+        var nonEmpty = FileSystem.readDirectory(workspaceRoot).exists(f -> !f.startsWith("."));
+        if (nonEmpty) {
+            window.showErrorMessage("To set up sample Haxe project, workspace must be either empty");
             return;
         }
 
-        var vscodeDir = workspaceRoot + "/.vscode";
-        var hxmls = findHxmls(workspaceRoot);
-        if (hxmls.length == 0) {
-            window.showErrorMessage("To set up Haxe project, workspace must be either empty or contain HXML files to choose from");
-        } else if (hxmls.length == 1) {
-            scaffoldVscodeSettings(vscodeDir, hxmls[0], hxmls);
-        } else {
-            createWorkspaceConfiguration(vscodeDir, hxmls);
-        }
-    }
-
-    function scaffoldEmpty(root:String) {
-        var scaffoldSource = context.asAbsolutePath("./scaffold/project");
-        copyRec(scaffoldSource, root);
+        copyRec(context.asAbsolutePath("./scaffold/project"), workspaceRoot);
         window.setStatusBarMessage("Haxe project scaffolded", 2000);
-    }
-
-    function findHxmls(root:String):Array<QuickPickItem> {
-        var hxmls:Array<QuickPickItem> = [];
-        function loop(path:String):Void {
-            var fullPath = root + "/" + path;
-            if (FileSystem.isDirectory(fullPath)) {
-                if (path == ".haxelib")
-                    return;
-                for (file in FileSystem.readDirectory(fullPath)) {
-                    if (file.endsWith(".hxml"))
-                        hxmls.push({label: file, description: path});
-                    else
-                        loop(if (path.length == 0) file else path + "/" + file);
-                }
-            }
-        }
-        loop("");
-        return hxmls;
-    }
-
-    static function getHxmlPath(item:QuickPickItem):String {
-        var path = item.description, file = item.label;
-        return if (path.length == 0) file else path + "/" + file;
-    }
-
-    function createWorkspaceConfiguration(vscodeDir:String, items:Array<QuickPickItem>) {
-        var pick = window.showQuickPick(items, {placeHolder: "Choose HXML file to use"});
-        pick.then(function(s:QuickPickItem):Void {
-            if (s != null)
-                scaffoldVscodeSettings(vscodeDir, s, items);
-        });
-    }
-
-    function scaffoldVscodeSettings(vscodeDir:String, item:QuickPickItem, items:Array<QuickPickItem>) {
-        var selectedHxml = getHxmlPath(item);
-
-        // update settings
-        var hxmls = items.map(function(item) return getHxmlPath(item));
-        // move selected on top
-        hxmls.remove(selectedHxml);
-        hxmls.insert(0, selectedHxml);
-        var config = workspace.getConfiguration("haxe");
-        config.update("displayConfigurations", [for (hxml in hxmls) [hxml]]).then(function(_) {
-            workspace.openTextDocument(vscodeDir + "/settings.json").then(function(doc) {
-                window.showTextDocument(doc);
-                window.showInformationMessage("Please check if " + selectedHxml + " is suitable for completion and modify haxe.displayConfigurations if needed.");
-            });
-        });
     }
 
     function copyRec(from:String, to:String):Void {
