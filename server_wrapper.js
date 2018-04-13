84dce11fcfd603c786d5bddb8e540609d1db18e1
Jens Fischer <jensfischer95@gmail.com>
2018-04-13 15:35:42 +0200

    Build the server to server/bin/ instead of bin/
    This simplifies standalone usage of the language server, see #171

--- server_wrapper.js
+++ server_wrapper.js
@@ -1,4 +1,4 @@
 // this is needed for starting server.js with child_process directly
 // and not blocking the server.js file for writing, so we can recompile it
 // while it's running
-require("./bin/server.js");
+require("./server/bin/server.js");
