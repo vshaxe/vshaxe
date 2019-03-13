deebcfee7c760da86550679e9e3b7f7555c9537e
Jens Fischer <jensfischer95@gmail.com>
2019-03-13 02:01:29 +0100

    Generate the hxformat.json schema automatically on build
    No need to update it manually anymore.

---
+++ schemas/SchemaGenerator.hx
@@ -0,0 +1,12 @@
+import haxe.Json;
+import sys.io.File;
+import formatter.config.FormatterConfig;
+import json2object.utils.JsonSchemaWriter;
+
+class SchemaGenerator {
+	static function main() {
+		var schema = new JsonSchemaWriter<FormatterConfig>().schema;
+		schema = Json.stringify(Json.parse(schema), "\t");
+		File.saveContent("schemas/hxformat-schema.json", schema);
+	}
+}
