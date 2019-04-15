9f07acfb469c1649a6184c46dfcf03226f351647
Jens Fischer <jensfischer95@gmail.com>
2019-04-15 23:40:50 +0200

    Regenerate formatter schema with latest json2object

--- schemas/SchemaGenerator.hx
+++ schemas/SchemaGenerator.hx
@@ -1,11 +1,13 @@
-import haxe.Json;
 import sys.io.File;
 import formatter.config.FormatterConfig;
 import json2object.utils.JsonSchemaWriter;
 
+using StringTools;
+
 class SchemaGenerator {
 	static function main() {
 		var schema = new JsonSchemaWriter<FormatterConfig>("\t").schema;
+		schema = schema.replace('"description"', '"markdownDescription"');
 		File.saveContent("schemas/hxformat-schema.json", schema);
 	}
 }
