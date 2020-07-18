6dbfe0cf0004f06a3625b81a7ee7d665607d72a8
George Corney <haxiomic@gmail.com>
2020-07-18 18:30:47 +0100

    Replace commas with dots when determining lib name from folder name
    Fixes #452

--- src/vshaxe/configuration/HaxeInstallation.hx
+++ src/vshaxe/configuration/HaxeInstallation.hx
@@ -96,7 +96,7 @@ class HaxeInstallation {
 		if (libraryBasePath == null || !FileSystem.exists(libraryBasePath) || !FileSystem.isDirectory(libraryBasePath)) {
 			return [];
 		}
-		return FileSystem.readDirectory(libraryBasePath).filter(file -> !file.startsWith(".")).map(file -> {name: file});
+		return FileSystem.readDirectory(libraryBasePath).filter(file -> !file.startsWith(".")).map(file -> {name: StringTools.replace(file, ',', '.')});
 	}
 
 	public function isWaitingForProvider():Bool {
