a5bb5e26ae578dd81461dc6dc5856dbcc5287d31
tobil4sk <tobil4sk@outlook.com>
2022-08-17 19:02:49 +0100

    Clean up Install script
    Remove boilerplate class

--- Install.hx
+++ Install.hx
@@ -1,51 +1,49 @@
-import sys.io.File;
 import sys.FileSystem;
+import sys.io.File;
 
 using StringTools;
 
-class Install {
-	static function main() {
-		// this is not hacky at all... but seems there's no way to make `vsce package` skip prepublish
-		var restorePackageJson = tempModification("package.json", function(content) {
-			return content.split("\n").map(line -> if (line.contains("vscode:prepublish")) "" else line).join("\n");
-		});
-		var restoreVscodeIgnore = tempModification(".vscodeignore", function(content) {
-			return content + [
-				"vscode-languageclient",
-				"semver",
-				"vscode-languageserver-protocol",
-				"vscode-jsonrpc",
-				"vscode-languageserver-types",
-			].map(lib -> '!node_modules/$lib/**/*').join("\n");
-		});
-		function restore() {
-			restorePackageJson();
-			restoreVscodeIgnore();
-		}
-		try {
-			Sys.command("vsce package");
+function main() {
+	// this is not hacky at all... but seems there's no way to make `vsce package` skip prepublish
+	final restorePackageJson = tempModification("package.json", function(content) {
+		return content.split("\n").map(line -> if (line.contains("vscode:prepublish")) "" else line).join("\n");
+	});
+	final restoreVscodeIgnore = tempModification(".vscodeignore", function(content) {
+		return content + [
+			"vscode-languageclient",
+			"semver",
+			"vscode-languageserver-protocol",
+			"vscode-jsonrpc",
+			"vscode-languageserver-types",
+		].map(lib -> '!node_modules/$lib/**/*').join("\n");
+	});
+	function restore() {
+		restorePackageJson();
+		restoreVscodeIgnore();
+	}
+	try {
+		Sys.command("vsce package");
 
-			// if it didn't fail, there should be a .vsix now
-			var vsixFiles = FileSystem.readDirectory(".").filter(file -> file.endsWith(".vsix"));
-			if (vsixFiles.length > 1) {
-				Sys.println('Multiple vsix files found: $vsixFiles');
-			} else {
-				Sys.command('code --install-extension ${vsixFiles[0]}');
-				FileSystem.deleteFile(vsixFiles[0]);
-			}
-		} catch (e) {
-			trace(e);
-			restore();
+		// if it didn't fail, there should be a .vsix now
+		final vsixFiles = FileSystem.readDirectory(".").filter(file -> file.endsWith(".vsix"));
+		if (vsixFiles.length > 1) {
+			Sys.println('Multiple vsix files found: $vsixFiles');
+		} else {
+			Sys.command('code --install-extension ${vsixFiles[0]}');
+			FileSystem.deleteFile(vsixFiles[0]);
 		}
+	} catch (e) {
+		trace(e);
 		restore();
 	}
+	restore();
+}
 
-	static function tempModification(file:String, modify:(content:String) -> String):() -> Void {
-		var originalContent = File.getContent(file);
-		var newContent = modify(originalContent);
-		File.saveContent(file, newContent);
-		return function() {
-			File.saveContent(file, originalContent);
-		};
-	}
+function tempModification(file:String, modify:(content:String) -> String):() -> Void {
+	final originalContent = File.getContent(file);
+	final newContent = modify(originalContent);
+	File.saveContent(file, newContent);
+	return function() {
+		File.saveContent(file, originalContent);
+	};
 }
